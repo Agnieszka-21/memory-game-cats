429c2cdd2f76f3fabfd8744bd39be6dd03726285
Agnieszka-21 <agnieszkabialekmusic@gmail.com>
2024-03-10 12:56:57 +0000

    Create an array of cards for the easy level, 6 pairs

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+// Array of cards for the easy level - 6 pairs
+const easyCards = [
+    {
+        name: 'cat1',
+        img: './images/easy/cat1.svg'
+    },
+    {
+        name: 'cat2',
+        img: './images/easy/cat2.svg'
+    },
+    {
+        name: 'cat3',
+        img: './images/easy/cat3.svg'
+    },
+    {
+        name: 'cat4',
+        img: './images/easy/cat4.svg'
+    },
+    {
+        name: 'cat5',
+        img: './images/easy/cat5.svg'
+    },
+    {
+        name: 'cat6',
+        img: './images/easy/cat6.svg'
+    },
+    {
+        name: 'cat1',
+        img: './images/easy/cat1.svg'
+    },
+    {
+        name: 'cat2',
+        img: './images/easy/cat2.svg'
+    },
+    {
+        name: 'cat3',
+        img: './images/easy/cat3.svg'
+    },
+    {
+        name: 'cat4',
+        img: './images/easy/cat4.svg'
+    },
+    {
+        name: 'cat5',
+        img: './images/easy/cat5.svg'
+    },
+    {
+        name: 'cat6',
+        img: './images/easy/cat6.svg'
+    }    
+];
+
+console.log(easyCards);
