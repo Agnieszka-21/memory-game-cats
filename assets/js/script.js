5623bc573f29b1cdac006abd5e317d28791df0e2
Agnieszka-21 <agnieszkabialekmusic@gmail.com>
2024-03-10 13:27:32 +0000

    Create arrays of cards for medium and difficult level

--- assets/js/script.js
+++ assets/js/script.js
@@ -120,3 +120,88 @@ const mediumCards = [
 ];
 console.log(mediumCards);
 
+// Array of cards for the difficult level - 10 pairs
+const difficultCards = [
+    {
+        name: 'silhouette1',
+        img: '../images/difficult/silhouette1.svg'
+    },
+    {
+        name: 'silhouette2',
+        img: '../images/difficult/silhouette2.svg'
+    },
+    {
+        name: 'silhouette3',
+        img: '../images/difficult/silhouette3.svg'
+    },
+    {
+        name: 'silhouette4',
+        img: '../images/difficult/silhouette4.svg'
+    },
+    {
+        name: 'silhouette5',
+        img: '../images/difficult/silhouette5.svg'
+    },
+    {
+        name: 'silhouette6',
+        img: '../images/difficult/silhouette6.svg'
+    },
+    {
+        name: 'silhouette7',
+        img: '../images/difficult/silhouette7.svg'
+    },
+    {
+        name: 'silhouette8',
+        img: '../images/difficult/silhouette8.svg'
+    },
+    {
+        name: 'silhouette9',
+        img: '../images/difficult/silhouette9.svg'
+    },
+    {
+        name: 'silhouette10',
+        img: '../images/difficult/silhouette10.svg'
+    },
+    {
+        name: 'silhouette1',
+        img: '../images/difficult/silhouette1.svg'
+    },
+    {
+        name: 'silhouette2',
+        img: '../images/difficult/silhouette2.svg'
+    },
+    {
+        name: 'silhouette3',
+        img: '../images/difficult/silhouette3.svg'
+    },
+    {
+        name: 'silhouette4',
+        img: '../images/difficult/silhouette4.svg'
+    },
+    {
+        name: 'silhouette5',
+        img: '../images/difficult/silhouette5.svg'
+    },
+    {
+        name: 'silhouette6',
+        img: '../images/difficult/silhouette6.svg'
+    },
+    {
+        name: 'silhouette7',
+        img: '../images/difficult/silhouette7.svg'
+    },
+    {
+        name: 'silhouette8',
+        img: '../images/difficult/silhouette8.svg'
+    },
+    {
+        name: 'silhouette9',
+        img: '../images/difficult/silhouette9.svg'
+    },
+    {
+        name: 'silhouette10',
+        img: '../images/difficult/silhouette10.svg'
+    }
+];
+console.log(difficultCards);
+
