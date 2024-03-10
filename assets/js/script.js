c38cd82e6c511fbeb309e75ccd8be8573a5c3dde
Agnieszka-21 <agnieszkabialekmusic@gmail.com>
2024-03-10 13:18:10 +0000

    Create an array of cards for the medium level, 8 pairs

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,52 +2,121 @@
 const easyCards = [
     {
         name: 'cat1',
-        img: './images/easy/cat1.svg'
+        img: '../images/easy/cat1.svg'
     },
     {
         name: 'cat2',
-        img: './images/easy/cat2.svg'
+        img: '../images/easy/cat2.svg'
     },
     {
         name: 'cat3',
-        img: './images/easy/cat3.svg'
+        img: '../images/easy/cat3.svg'
     },
     {
         name: 'cat4',
-        img: './images/easy/cat4.svg'
+        img: '../images/easy/cat4.svg'
     },
     {
         name: 'cat5',
-        img: './images/easy/cat5.svg'
+        img: '../images/easy/cat5.svg'
     },
     {
         name: 'cat6',
-        img: './images/easy/cat6.svg'
+        img: '../images/easy/cat6.svg'
     },
     {
         name: 'cat1',
-        img: './images/easy/cat1.svg'
+        img: '../images/easy/cat1.svg'
     },
     {
         name: 'cat2',
-        img: './images/easy/cat2.svg'
+        img: '../images/easy/cat2.svg'
     },
     {
         name: 'cat3',
-        img: './images/easy/cat3.svg'
+        img: '../images/easy/cat3.svg'
     },
     {
         name: 'cat4',
-        img: './images/easy/cat4.svg'
+        img: '../images/easy/cat4.svg'
     },
     {
         name: 'cat5',
-        img: './images/easy/cat5.svg'
+        img: '../images/easy/cat5.svg'
     },
     {
         name: 'cat6',
-        img: './images/easy/cat6.svg'
+        img: '../images/easy/cat6.svg'
     }    
 ];
-
 console.log(easyCards);
+
+// Array of cards for the medium level - 8 pairs
+const mediumCards = [
+    {
+        name: 'black-cat1',
+        img: '../images/medium/black-cat1.svg'
+    },
+    {
+        name: 'black-cat2',
+        img: '../images/medium/black-cat2.svg'
+    },
+    {
+        name: 'black-cat3',
+        img: '../images/medium/black-cat3.svg'
+    },
+    {
+        name: 'black-cat4',
+        img: '../images/medium/black-cat4.svg'
+    },
+    {
+        name: 'black-cat5',
+        img: '../images/medium/black-cat5.svg'
+    },
+    {
+        name: 'black-cat6',
+        img: '../images/medium/black-cat6.svg'
+    },
+    {
+        name: 'black-cat7',
+        img: '../images/medium/black-cat7.svg'
+    },
+    {
+        name: 'black-cat8',
+        img: '../images/medium/black-cat8.svg'
+    },
+    {
+        name: 'black-cat1',
+        img: '../images/medium/black-cat1.svg'
+    },
+    {
+        name: 'black-cat2',
+        img: '../images/medium/black-cat2.svg'
+    },
+    {
+        name: 'black-cat3',
+        img: '../images/medium/black-cat3.svg'
+    },
+    {
+        name: 'black-cat4',
+        img: '../images/medium/black-cat4.svg'
+    },
+    {
+        name: 'black-cat5',
+        img: '../images/medium/black-cat5.svg'
+    },
+    {
+        name: 'black-cat6',
+        img: '../images/medium/black-cat6.svg'
+    },
+    {
+        name: 'black-cat7',
+        img: '../images/medium/black-cat7.svg'
+    },
+    {
+        name: 'black-cat8',
+        img: '../images/medium/black-cat8.svg'
+    }
+];
+console.log(mediumCards);
+
