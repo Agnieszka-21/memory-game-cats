8b08b7a699b1438c1d38a6e741d24fb2c7619794
Agnieszka-21 <agnieszkabialekmusic@gmail.com>
2024-03-17 21:53:21 +0000

    Add a modal with its mobile styling & basic JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -423,12 +423,13 @@ purring.addEventListener('click', purringOnOff);
 
 const soundIcon = document.getElementById('sound-icon');
 
+let sound = new Audio('../assets/audio/purring.mp3');
+sound.classList.add('not-playing');
+
 function purringOnOff() {
-    let sound = false;
     console.log('Purring on/off');
 
-    if (sound === false) {
-        sound = new Audio('../assets/audio/purring.mp3');
+    if (sound.classList = 'not-playing') {
         sound.play();
         // loop the audio
         sound.addEventListener('ended', function() {
@@ -436,31 +437,43 @@ function purringOnOff() {
             this.play();
         }, false);
         soundIcon.setAttribute('src', '../assets/images/sound-off.svg');
-        console.log('Purring has been turned on')
-    }
-}    
-
-//     soundIcon.setAttribute('src', '../assets/images/sound-off');
-/*
-    sound.addEventListener('playing', function() {
-        sound.pause();
-    })
-
-
-    sound.addEventListener('ended', function() {
-        this.currentTime = 0;
-        this.play();
-    }, false);    
-    */
-
-/*
-    else if (sound.playing = true) {
-        pauseSound();
+        sound.classList.remove('not-playing');
+        console.log('Purring has been turned on');
     } else {
-        playSound();
-    }
-*/    
+        sound.pause();
+        soundIcon.setAttribute('src', '../assets/images/sound-on.svg'); 
+        sound.classList.add('not-playing');
+        console.log('Purring has been turned off');
+    }    
+}    
     
+
+// MODAL copied
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 
 
