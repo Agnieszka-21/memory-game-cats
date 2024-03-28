e002cd7d87d7e5822ea08e9f6abe9cf37c68bed5
Agnieszka-21 <agnieszkabialekmusic@gmail.com>
2024-03-28 15:03:15 +0000

    Detect device to display the gif as video or static image

--- assets/js/script.js
+++ assets/js/script.js
@@ -784,3 +784,35 @@ function trapFocus(modalFireworksElement) {
         }
     });
 }
+
+/**
+ * Detect whether the user's device is a mobile device to adjust the landing page gif:
+ * <video> on laptops and larger screens, <img> on touchscreen devices where autoplay is blocked
+ */
+
+// The following code has been copied from MDN documentation
+let hasTouchScreen = false;
+if ("maxTouchPoints" in navigator) {
+    hasTouchScreen = navigator.maxTouchPoints > 0;
+} else if ("msMaxTouchPoints" in navigator) {
+    hasTouchScreen = navigator.msMaxTouchPoints > 0;
+} else {
+    const mQ = matchMedia?.("(pointer:coarse)");
+    if (mQ?.media === "(pointer:coarse)") {
+        hasTouchScreen = !!mQ.matches;
+    } else if ("orientation" in window) {
+        hasTouchScreen = true; // deprecated, but good fallback
+    } else {
+        // Only as a last resort, fall back to user agent sniffing
+        const UA = navigator.userAgent;
+        hasTouchScreen =
+            /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+            /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
+    }
+}
+
+if (hasTouchScreen) {
+    // The above line is the end of MDN documentation code
+    document.getElementById('gif-video').classList.add('hide');
+    document.getElementById('gif-img').classList.remove('hide');
+}
